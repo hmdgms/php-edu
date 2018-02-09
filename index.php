eee32d7d9aa036ad084de9d54feb3c6ed9e48ca3
hmdgms <hamdigumus@gmail.com>
2018-02-09 10:13:18 +0300

    php derslerine başladık

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+  $metin="2556risindeki metin";
+  $metin2='tek tırnak içerisindeki metin';
+  echo $metin;
+  echo "<br>";
+  echo $metin2;
+  echo "<hr>";
+  $sayi=13;
+  echo $metin * $sayi;
+  echo "<hr>";
+  $ondalikli=536.63;
+  echo $ondalikli;
+
+  //bu tek satırlık bir yorum
+
+  /*
+bu
+da
+çok
+satırlı
+bir yorum
+  */
+
+  //diziler
+
+  //object
+
+  //sabitler
+
+  //resources
+?>
